1712ebde8331aa5f33fbc9eed5045de50efbaf63
CaioPita <caio-gama@outlook.com>
2025-03-26 19:05:57 -0300

    Add files via upload

---
+++ aula04.py
@@ -0,0 +1,9 @@
+def nota():
+    
+    nome= input("Digite seu nome: ")    
+    idade: int(input("Digite sua idade: ")) 
+    nota: float(input("Digite sua nota: "))
+
+    print(f"Seu nome é : ",nome)
+    print(f"Sua idade é:  ",idade)
+    pri
