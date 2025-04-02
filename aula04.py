2daebfdcd70d0dd2deda854187038669b8ecd123
CaioPita <caio-gama@outlook.com>
2025-04-02 17:13:00 -0300

    Delete aula04.py

--- aula04.py
+++
@@ -1,9 +0,0 @@
-def nota():
-    
-    nome= input("Digite seu nome: ")    
-    idade: int(input("Digite sua idade: ")) 
-    nota: float(input("Digite sua nota: "))
-
-    print(f"Seu nome é : ",nome)
-    print(f"Sua idade é:  ",idade)
-    pri
